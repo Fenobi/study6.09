3fe61cad5c1e7049396cca0b1537a55d84e4a401
Fenobi <13667739122@qq.com>
2022-06-09 23:21:33 +0800

    添加项目文件。

---
+++ fu.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+//#include <stdio.h>
+//int main()
+//{
+//	int i = 1;
+//	int n = 0;
+//	int j = 0;
+//	for (i = 1; i < 5; i++)
+//	{
+//		scanf("%d ", &n);
+//		for (j = 2; j < n; j++)
+//		{
+//			if (n % j == 0)
+//			{
+//				break;
+//			}
+//		}
+//		if (j == n)
+//		{
+//			printf("%d ", n);
+//		}
+//
+//	}
+//	return 0;
+//}
